3323a0920d389979f4d74fbb4bd8aaf169217536
Ahn-uisung <a28460190@gmail.com>
2024-11-15 21:41:35 +0900

    11-15

---
+++ Assets/AES/Script/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+
+
+    public float damage;
+    public int per;
+
+    public void init(float damage,int per)
+    {
+        this.damage = damage;
+        this.per = per;
+    }
+}
